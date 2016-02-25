120223a12e85bd44326e261ac9cf407096013f33
Topani Portales <topani.dev@gmail.com>
2016-02-24 23:51:42 -0600

    created escape character trial program

---
+++ escapeSequences/escape.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+	cout << "Newline\n";
+	cout << "Newline with endl" << endl << endl;;
+
+	cout << "This is a long sentence with a \r carriage return" << endl << endl;
+
+	cout << "This is a long \b sentence\bwith\b some back\bspaces in it" << endl;
+	return 0;
+}

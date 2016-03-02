4734cd21910d6c83605453221817f9c46c0988a3
Topani Portales <topani.dev@gmail.com>
2016-03-01 23:50:37 -0600

    Completed ex 1.1 and created makefile

---
+++ exercises/ch01/ex1.1.cpp
@@ -0,0 +1,17 @@
+/*****
+**
+** Page 28
+** Exercise 1.1
+** Create a program that will pring out the text "Hello World" to your screen.
+**
+*****/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << endl << "Hello World" << endl << endl;
+
+}
